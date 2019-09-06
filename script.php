6334d83408ca7bd485e17e2bee5303c343f41d00
KilliannH <killiannhervagault@gmail.com>
2019-09-06 15:34:22 +0200

    from txt to json

---
+++ script.php
@@ -0,0 +1,41 @@
+<?php
+
+$properties = ['Request: USER ', 'Request: PASS '];
+
+$users = [];
+$passwords = [];
+
+$jsons = [];
+
+$file_lines = [];
+
+if ($fh = fopen('export_wireshark_2.txt', 'r')) {
+    while (!feof($fh)) {
+        $line = fgets($fh);
+        array_push($file_lines, $line);
+    }
+    fclose($fh);
+}
+
+for ($i = 0; $i < sizeof($file_lines); $i++) {
+    if (strstr( $file_lines[$i], $properties[0])) {
+        $userStr = strstr($file_lines[$i], $properties[0]);
+        $userArr = explode('USER ', $userStr);
+        $user = $userArr[1];
+        $user = rtrim($user, "\r\n");
+        array_push($users, $user);
+    } else if (strstr( $file_lines[$i], $properties[1])) {
+        $passStr = strstr($file_lines[$i], $properties[1]);
+        $passArr = explode('PASS ', $passStr);
+        $pass = $passArr[1];
+        $pass = rtrim($pass, "\r\n");
+        array_push($passwords, $pass);
+    }
+}
+
+for($i = 0; $i < sizeof($users); $i++) {
+    $jsons[$i] = array($users[$i] => $passwords[$i]);
+}
+
+$myjson = json_encode($jsons);
+echo $myjson;
